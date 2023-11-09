83a217ad5be79c9a7055070a127e12ca89a00725
brian.mulier <bmmulier@hotmail.fr>
2023-11-09 13:12:06 +0100

    fix(core): null-safe uris in storage + fixed kestra path returned by put method

--- src/test/java/io/kestra/storage/minio/MinioStorageTest.java
+++ src/test/java/io/kestra/storage/minio/MinioStorageTest.java
@@ -1,14 +1,12 @@
 package io.kestra.storage.minio;
 
 import io.kestra.core.storage.StorageTestSuite;
-import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
 
-@MicronautTest
 class MinioStorageTest extends StorageTestSuite {
     @Inject
     MinioClientFactory clientFactory;
