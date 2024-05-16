b02b500eac2d39a0873188e6c4e3742ecf0029d7
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-05-16 14:18:25 +0200

    feat: add support for AWS credentials
    related-to: kestra-io/kestra#2914

--- src/main/java/io/kestra/storage/minio/MinioClientFactory.java
+++ src/main/java/io/kestra/storage/minio/MinioClientFactory.java
@@ -1,6 +1,14 @@
 package io.kestra.storage.minio;
 
 import io.minio.MinioClient;
+import io.minio.credentials.AwsConfigProvider;
+import io.minio.credentials.AwsEnvironmentProvider;
+import io.minio.credentials.ChainedProvider;
+import io.minio.credentials.IamAwsProvider;
+import io.minio.credentials.MinioEnvironmentProvider;
+import io.minio.credentials.Provider;
+import io.minio.credentials.StaticProvider;
+import org.jetbrains.annotations.NotNull;
 
 public class MinioClientFactory {
 
@@ -8,15 +16,16 @@ public class MinioClientFactory {
         try {
             MinioClient.Builder bdr;
             bdr = MinioClient.builder()
-                    .endpoint(config.getEndpoint(), config.getPort(), config.isSecure());
+                .endpoint(config.getEndpoint(), config.getPort(), config.isSecure());
+
             if (config.getRegion() != null) {
                 bdr.region(config.getRegion());
             }
-            if (config.getAccessKey() != null && config.getSecretKey() != null) {
-                bdr.credentials(config.getAccessKey(), config.getSecretKey());
-            }
+            
+            bdr.credentialsProvider(getCredentialProvider(config));
 
             MinioClient build = bdr.build();
+
             if (config.isVhost()) {
                 build.enableVirtualStyleEndpoint();
             }
@@ -25,5 +34,22 @@ public class MinioClientFactory {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @NotNull
+    public static Provider getCredentialProvider(final MinioConfig config) {
+        Provider provider;
+        if (config.getAccessKey() != null && config.getSecretKey() != null) {
+            provider = new StaticProvider(config.getAccessKey(), config.getSecretKey(), null);
+        } else {
+            // Otherwise, by default configure the credential chains.
+            provider = new ChainedProvider(
+                new AwsEnvironmentProvider(),
+                new MinioEnvironmentProvider(),
+                new AwsConfigProvider(null, null),
+                new IamAwsProvider(null, null)
+            );
+        }
+        return provider;
     }
 }
