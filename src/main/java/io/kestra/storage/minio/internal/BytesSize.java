e8c84f1d612c9dc26d41a00a09cf7f70cf61c458
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-05-16 15:22:40 +0200

    fix(minio): fix regression on partSize property as readable byte-size
    Previously partSize property convertion was handled by Micronaut
    converter using the ReadableBytes annotation.

---
+++ src/main/java/io/kestra/storage/minio/internal/BytesSize.java
@@ -0,0 +1,65 @@
+package io.kestra.storage.minio.internal;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+import java.util.Locale;
+import java.util.OptionalLong;
+
+/**
+ * Wrapper for long representation of a byte size.
+ *
+ * @param value a string bytes size.
+ */
+
+public record BytesSize(long value) {
+
+    private static final String KILOBYTES = "KB";
+    private static final String MEGABYTES = "MB";
+    private static final String GIGABYTES = "GB";
+    private static final int KB_UNIT = 1024;
+
+    public BytesSize(final String value) {
+        this(convert(value).orElseThrow(
+            () -> new IllegalArgumentException(String.format("Cannot convert '%s' to long bytes size.", value))));
+    }
+
+    /**
+     * Factory method for constructing a new {@link BytesSize} object from a given string.
+     *
+     * @param value  a human-readable bytes size, e.g. 1KB, 1MB, 1GB.
+     * @return a new {@link BytesSize}.
+     */
+    @JsonCreator
+    public static BytesSize of(final String value) {
+        return new BytesSize(value);
+    }
+
+    private static OptionalLong convert(final String object) {
+        if (object == null || object.isEmpty()) {
+            return OptionalLong.empty();
+        }
+        final String value = object.toUpperCase(Locale.ENGLISH);
+        try {
+            long size;
+            if (value.endsWith(KILOBYTES)) {
+                long numberPart = parseSizeWithUnit(value);
+                size = numberPart * KB_UNIT;
+            } else if (value.endsWith(MEGABYTES)) {
+                long numberPart = parseSizeWithUnit(value);
+                size = numberPart * KB_UNIT * KB_UNIT;
+            } else if (value.endsWith(GIGABYTES)) {
+                long numberPart = parseSizeWithUnit(value);
+                size = numberPart * KB_UNIT * KB_UNIT * KB_UNIT;
+            } else {
+                size = Long.parseLong(value);
+            }
+            return OptionalLong.of(size);
+        } catch (NumberFormatException e) {
+            return OptionalLong.empty();
+        }
+    }
+
+    private static long parseSizeWithUnit(final String value) {
+        return Long.parseLong(value.substring(0, value.length() - 2));
+    }
+}
