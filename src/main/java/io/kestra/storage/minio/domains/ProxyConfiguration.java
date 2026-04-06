3a9292eac11ad3bef1a12dfae95a9da143ee5bd7
François Delbrayelle <fdelbrayelle@kestra.io>
2026-04-06 10:08:36 +0200

    feat: add @PluginProperty group annotations from taxonomy

--- src/main/java/io/kestra/storage/minio/domains/ProxyConfiguration.java
+++ src/main/java/io/kestra/storage/minio/domains/ProxyConfiguration.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.extern.jackson.Jacksonized;
+import io.kestra.core.models.annotations.PluginProperty;
 
 @Getter
 @Builder(toBuilder = true)
@@ -15,14 +16,18 @@ public class ProxyConfiguration {
     private Proxy.Type type = Proxy.Type.DIRECT;
 
     @Schema(title = "The address of the proxy server.")
+    @PluginProperty(group = "advanced")
     private String address;
 
     @Schema(title = "The port of the proxy server.")
+    @PluginProperty(group = "connection")
     private Integer port;
 
     @Schema(title = "The username for proxy authentication.")
+    @PluginProperty(group = "connection")
     private String username;
 
     @Schema(title = "The password for proxy authentication.")
+    @PluginProperty(group = "connection")
     private String password;
 }
